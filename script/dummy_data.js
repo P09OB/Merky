13e765cce318d877b141e58c7c8359d9f6ec3e8b
Sok4h <mrsokaxd@gmail.com>
2021-11-13 17:53:06 -0500

    dummy data file

---
+++ script/dummy_data.js
@@ -0,0 +1,24 @@
+
+//añade mas usuarios usando esa estructura, poniendo nuevas imagenes de perfil 
+const users = [
+    {   
+        id:math.random(),
+        userName:"Pepito",
+        profilePicture:"https://upload.wikimedia.org/wikipedia/commons/1/18/Mark_Zuckerberg_F8_2019_Keynote_%2832830578717%29_%28cropped%29.jpg",
+        
+    }
+]
+
+//añade mas productos  usando esa estructura, poniendo nuevas imagenes de productos 
+const products = [
+
+    {
+        title: "Papas margarita", 
+        description: "las vende el pibe", 
+        id: math.random(), 
+        img: "https://colsubsidio.vteximg.com.br/arquivos/ids/166452-1200-1200/7702189053770.jpg?v=637299796871330000", 
+        rating: 5, 
+        price: 4000,
+
+    }
+]
